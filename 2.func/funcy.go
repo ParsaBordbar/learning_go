5295b248d57db055343d9f60f3880540caaad351
ParsaBordbar <parsab71@gmail.com>
2024-11-26 22:24:12 +0330

    feat: prime_number func in funcy also refactor dir structure

---
+++ 2.func/funcy.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func prime_number (number int) (isPrime bool) {
+
+	isPrime = true
+	for i := 2 ; i <= number/2 ; i++ {
+		if number % i == 0 {
+			isPrime = false
+			break
+		} 
+	}  
+	if isPrime {
+		return
+	}
+	return
+}
+
+func main() {
+	var number int;
+	fmt.Scanf("%v" , &number)
+	fmt.Println(prime_number(number))
+}
