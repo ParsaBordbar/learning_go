9e901748eafb2a1906de83c4b7301b08b82552d6
Max Edison <amirhossein.heidari.email@gmail.com>
2024-11-28 03:42:51 +0330

    Add directories to Python module.

--- readytogo/python/python.go
+++ readytogo/python/python.go
@@ -7,7 +7,7 @@ import (
 )
 
 func Python() {
-
+	//get project name
 	var proj_name string
 	fmt.Print("Enter the Project name: ")
 	fmt.Scanf("%s", &proj_name)
@@ -22,7 +22,18 @@ func Python() {
 	}
 
 	file_names := [2]string{"main.py", "requirements.txt"}
+	dir_names := []string{"src", "tests", "data", "docs"}
 
+	// make directories
+	for _, dir := range dir_names {
+		if _, err := os.Stat(dir); os.IsNotExist(err) {
+			if err := os.Mkdir(dir, 0755); err != nil {
+				log.Printf("Failed to create directory %s: %v", dir, err)
+			}
+		}
+	}
+
+	// make files
 	for i := 0; i < 2; i++ {
 		if _, err := os.Stat(file_names[i]); err != nil {
 			os.Create(file_names[i])
