5d51d4d57e608a5752ababc6bf71f1b7b6db8357
ParsaBordbar <parsab71@gmail.com>
2024-11-28 15:43:34 +0330

    refactor: main module readto.go add file_tree

--- readytogo/readyto.go
+++ readytogo/readyto.go
@@ -3,13 +3,19 @@ package main
 import (
 	"fmt"
     "readytogo/express" 
+	"readytogo/file_tree"
 )
 
 func main() {
 	var kind string
 	fmt.Scanf("%s", &kind)
 	switch kind {
-	case "express":
-		express.Express()		
+		case "express":
+			express.Express()		
+		case "tree": {
+			root := "."
+			fmt.Println("File Tree:")
+			file_tree.PrintTree(root, "  ")
+		}
 	}
 }
