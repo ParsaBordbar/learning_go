b8a6371d3f93b84bc0234bc9806e5e2aecfe0854
ParsaBordbar <parsab71@gmail.com>
2024-11-28 15:43:01 +0330

    feat: npm init

--- readytogo/express/express.go
+++ readytogo/express/express.go
@@ -4,14 +4,28 @@ import (
 	"fmt"
     "os"
     "log"
+    "os/exec"
 )
 
+// To implement: enums and arguments --flags for pure & ts
 func Express() {
+    
+    // type flag string
+    // const (
+    //     pure    Flag = "pure"
+    //     ts      Flag = "ts"
+    // )
+
+    // To implement: change output to desired route in next update
+    if err := os.Chdir("output"); err != nil {
+        log.Fatalf("Failed to change directory to %s: %v", "output", err)
+    }
 
 	var proj_name string
 	fmt.Print("Enter the Project name: ")
 	fmt.Scanf("%s", &proj_name)
-
+    
+    // Create project directory if not exists
     if _, err := os.Stat(proj_name); os.IsNotExist(err) {
         if err := os.Mkdir(proj_name, 0755); err != nil {
             log.Fatalf("Failed to create project directory: %v", err)
@@ -21,10 +35,26 @@ func Express() {
         log.Fatalf("Failed to change directory to %s: %v", proj_name, err)
     }
 
+    // npm package init
+    terminal := exec.Command("npm", "init", "-y") 
+    terminal.Stdout = os.Stdout
+    terminal.Run()
 
+    // Install express, nodemon, joi, mongoose, helmet, morgan, axios
+    // var packages = [7] string {"express", "nodemon", "joi", "mongoose", "helmet", "morgan", "axios"} 
+    // for _, package := range packages {
+
+    // }
+    terminal2 := exec.Command("npm", "i" ,"express nodemon joi mongoose helmet morgan axios") 
+    terminal2.Stdout = os.Stdout
+    terminal2.Run()
+    
+
+    // Create Sub-dir and files if not exist
     dir_names := [5] string {"routes", "controllers", "middlewares", "models", "configs"}
 	file_names := [2] string {"server.js", "app.js"}
 
+    // Create sub-dir
     for _, dir := range dir_names {
         if _, err := os.Stat(dir); os.IsNotExist(err) {
             if err := os.Mkdir(dir, 0755); err != nil {
@@ -32,7 +62,7 @@ func Express() {
             }
         }
     }
-
+    // Create Files
 	for _, file := range file_names {
 		if _, err := os.Stat(file); os.IsNotExist(err) {
 			if _, err := os.Create(file); err != nil {
@@ -41,6 +71,7 @@ func Express() {
 		}
 	}
 
+    // Go to dir routes and create routes.js if not exist
     if err := os.Chdir("routes"); err != nil {
         log.Fatalf("Failed to change directory to routes: %v", err)
     }
