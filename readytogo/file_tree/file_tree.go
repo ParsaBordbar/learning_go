ff179abf2a32a72351b0c4f8c05c5338afb20245
ParsaBordbar <parsab71@gmail.com>
2024-11-28 15:38:05 +0330

    feat: file_tree module prints dirctury tree of the spesific route

---
+++ readytogo/file_tree/file_tree.go
@@ -0,0 +1,22 @@
+package file_tree
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func PrintTree(root string, indent string) {
+	entries, err := os.ReadDir(root)
+	if err != nil {
+		fmt.Println("Error reading directory:", err)
+		return
+	}
+
+	for _, entry := range entries {
+		fmt.Println(indent + "|-- " + entry.Name())
+		if entry.IsDir() {
+			PrintTree(filepath.Join(root, entry.Name()), indent+"    ")
+		}
+	}
+}
